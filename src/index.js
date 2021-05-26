cd17da3a473eae60de8965f49d465e7fdc9fa58c
dnodolf1212 <dnodolf@gmail.com>
2021-05-25 21:55:36 -0700

    use eventlistener for image mouse-over. Change heading text.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const wrapper = document.querySelector(".wrapper");
 
 document.addEventListener("DOMContentLoaded", () => {
+  console.log("We're writing javascript!!");
 
+  const pic = document.querySelector(".myImg");
+  const imageText = document.querySelector(".hover-text");
+  console.log(imageText)
+  console.log(pic)
+  pic.addEventListener("mouseenter", (e) => {
+    console.log(imageText.innerText);
+    imageText.innerText = "I hail from Madison, WI!";
+  })
 
 })

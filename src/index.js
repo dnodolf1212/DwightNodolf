fc356baa32b61cd6c27c2d720f1987da5a2b4e42
dnodolf1212 <dnodolf@gmail.com>
2021-06-10 08:34:36 -0700

    create html and fetch call to embed Dev.to blog.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const api = new ApiService("https://dev.to/api/articles?");
+const api = "https://dev.to/api/articles?";
 const wrapper = document.querySelector(".wrapper");
 const blogPost = document.querySelector("#blog");
   //Dev.to username and api
   const username = "notmuchnotmuch";
 
   createNode = (el) => {
-    return document.creeateElement(el);
+    return document.createElement(el);
   };
 
   append = (parentEl, childEl) => {
@@ -15,7 +15,36 @@ const blogPost = document.querySelector("#blog");
   const ul = createNode('ul');
   ul.classList.add("blog-feed");
 
+  makeURL = (userName) => {
+    return`${api}username=${userName}`;
+  }
+
+  const myURL = makeURL(username);
+  fetch(myURL)
+  .then(res => res.json())
+  .then(posts => {
+    posts.length = 3;
+
+    posts.forEach(post => {
+      let li = createNode('li'), a = createNode('a');
+      let h2 = createNode('h2'), p = createNode('p');
+      let small = createNode('small');
+
+      a.target = "_blank";
+      p.classList.add("to-connecter");
+      a.href = post.url;
+      a.innerText = post.title;
+      p.innerText = post.description;
+
+      append(h2, a);
+      append(li, h2);
+      append(ul, li);
+    })
+    append(blogPost, ul);
+  })
+
 document.addEventListener("DOMContentLoaded", () => {
+  
   console.log("We're writing javascript!!");
 
   const pic = document.querySelector(".myImg");
@@ -28,7 +57,7 @@ document.addEventListener("DOMContentLoaded", () => {
     imageText.append.newText
   });
 
-  
+
 
 })
 
